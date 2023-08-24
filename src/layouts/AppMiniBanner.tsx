3170df1ae707008ddaf8b05e1d0890eb7958481e
Mooncake Eater <mooncakefi@proton.me>
2023-08-24 23:21:02 +0300

    Updated banner

--- src/layouts/AppMiniBanner.tsx
+++ src/layouts/AppMiniBanner.tsx
@@ -24,7 +24,7 @@ const AppMiniBanner = () => {
           <Trans>🔒 AUDITS UNDERWAY 🔒</Trans>
         </Link>
         <Link href="https://guild.xyz/mooncakefi" color="inherit" target="_blank" mr={6}>
-          <Trans>👨‍👩‍👦 JOIN OUR GUILD AND GET SPECIAL DISCORD ACCESS 👨‍👩‍👦</Trans>
+          <Trans>💪 JOIN OUR GUILD 💪</Trans>
         </Link>
         <Link
           href="https://docs.mooncake.fi/token-overview/usdemcake-pre-mine-protocol-token"
@@ -43,7 +43,26 @@ const AppMiniBanner = () => {
           <Trans>🔒 AUDITS UNDERWAY 🔒</Trans>
         </Link>
         <Link href="https://guild.xyz/mooncakefi" color="inherit" target="_blank" mr={6}>
-          <Trans>👨‍👩‍👦 JOIN OUR GUILD AND GET SPECIAL DISCORD ACCESS 👨‍👩‍👦</Trans>
+          <Trans>💪 JOIN OUR GUILD 💪</Trans>
+        </Link>
+        <Link
+          href="https://docs.mooncake.fi/token-overview/usdemcake-pre-mine-protocol-token"
+          color="inherit"
+          target="_blank"
+          mr={6}
+        >
+          <Trans>🥮 $MCAKE PRE-MINE IS LIVE 🥮</Trans>
+        </Link>
+        <Link
+          href="https://docs.mooncake.fi/protocol/audits"
+          color="inherit"
+          target="_blank"
+          mr={6}
+        >
+          <Trans>🔒 AUDITS UNDERWAY 🔒</Trans>
+        </Link>
+        <Link href="https://guild.xyz/mooncakefi" color="inherit" target="_blank" mr={6}>
+          <Trans>💪 JOIN OUR GUILD 💪</Trans>
         </Link>
       </Marquee>
     </Box>
